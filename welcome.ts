f7cdf04fe512b78fdfe20d1d069254bc3613661d
ShunOmine <shun12baseball@gmail.com>
2020-05-16 08:48:18 +0900

    add: urlによってviewを変えるように

--- welcome.ts
+++ welcome.ts
@@ -3,31 +3,66 @@ import { serve } from 'http/server.ts';
 const s = serve({ port: 8000 });
 console.log('http://localhost:8000');
 
-for await (const req of s) {
-  req.respond({
+const Template = (children?: any) => {
+  return `
+    <html>
+      <head>
+        <title>Deno tutorial</title>
+        <style>
+          html {margin: 0;padding: 0;}
+          body {margin: 0;padding: 0;color: #333;}
+          .__deno_box {padding: 10px;}
+        </style>
+      </head>
+      <body>
+        ${children}
+      </body>
+    </html>
+  `
+}
+
+const home = (req: any) => {
+  return req.respond({
     status: 200,
-    headers: new Headers({
-      'Content-Type': 'text/html; charset=UTF-8'
-    }),
-    body: `
-      <html>
-        <head>
-          <title>Deno tutorial</title>
-          <style>
-            html {margin: 0;padding: 0;}
-            body {margin: 0;padding: 0;color: #333;}
-            .__deno_box {padding: 10px;}
-          </style>
-        </head>
-        <body>
-          <div class="__deno_box">
-            <h1>Hello World!!</h1>
-            <h2>Welcome to Deno!!</h2>
-            <p>URL: ${req.url}</p>
-            <p>method: ${req.method}</p>
-          </div>
-        </body>
-      </html>
-    `,
+    body: Template(`
+      <div class="__deno_box">
+        <h1>Hello World!!</h1>
+        <h2>Welcome to Deno!!</h2>
+        <p>URL: ${req.url}</p>
+        <p>method: ${req.method}</p>
+      </div>
+    `),
   });
+}
+
+const about = (req: any) => {
+  return req.respond({
+    status: 200,
+    body: Template(`
+      <div class="__deno_box">
+        <h1>About</h1>
+        <p>URL: ${req.url}</p>
+        <p>method: ${req.method}</p>
+      </div>
+    `),
+  });
+}
+
+const not_found = (req: any) => {
+  return req.respond({
+    status: 404,
+    body: 'not found.'
+  });
+}
+
+for await (const req of s) {
+  if (req.url === '/') {
+    home(req);
+  } 
+  if (req.url === '/about') {
+    about(req);
+  }
+  if (req.url !== '/' && req.url !== '/about') {
+    not_found(req);
+  }
 }
