4aee4e1158ee93fe0a55a5d858207f0075cb5e80
ShunOmine <shun12baseball@gmail.com>
2020-05-16 07:19:35 +0900

    create: tsconfig.json import_map.json

--- welcome.ts
+++ welcome.ts
@@ -1,6 +1,8 @@
-import { serve } from 'https://deno.land/std@0.50.0/http/server.ts';
+import { serve } from 'http/server.ts';
+
 const s = serve({ port: 8000 });
 console.log('http://localhost:8000');
+
 for await (const req of s) {
   req.respond({ body: 'Hello World\n' })
 }
